910e1a0864067252dccc24edba73c988ec9e4061
Maxime Steinhausser <maxime.steinhausser@gmail.com>
2016-10-03 23:09:55 +0200

    Fix missing licence header for Handler

--- src/Handler/Handler.php
+++ src/Handler/Handler.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Manala package.
+ *
+ * (c) Manala <contact@manala.io>
+ *
+ * For the full copyright and license information, please refer to the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Manala\Handler;
 
 /**
@@ -14,7 +23,7 @@ interface Handler
      * Handles the Build through running sub processes.
      *
      * @param callable|null $callback A function to be executed during the handling,
-     *                                e.g. for writing to the output of the caller command.
+     *                                e.g. for writing to the output of the caller command
      *
      * @return mixed
      */
