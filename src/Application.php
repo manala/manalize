9b153d219a652fb1d13674e415bb11bcbc6bf331
Robin Chalas <robin.chalas@gmail.com>
2016-10-19 16:39:49 +0200

    Rollback version to 2.0.0

--- src/Application.php
+++ src/Application.php
@@ -18,7 +18,7 @@ use Symfony\Component\Console\Application as BaseApplication;
  */
 final class Application extends BaseApplication
 {
-    const VERSION = '0.2.1-DEV';
+    const VERSION = '0.2.0';
     const REPOSITORY_NAME = 'manala/manalize';
 
     public function __construct()
