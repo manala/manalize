dcc316c0a4011ad39294f67ccb798710fab9a9d8
Robin Chalas <robin.chalas@gmail.com>
2018-08-13 11:26:29 +0200

    Bump version to 0.9.0

--- src/Application.php
+++ src/Application.php
@@ -18,7 +18,7 @@ use Symfony\Component\Console\Application as BaseApplication;
  */
 final class Application extends BaseApplication
 {
-    const VERSION = '0.8.1-DEV';
+    const VERSION = '0.9.0';
     const REPOSITORY_NAME = 'manala/manalize';
 
     public function __construct()
