d3578b48d0a451e4adaea3026fe36b694c6d3187
Maxime Steinhausser <maxime.steinhausser@elao.com>
2018-11-19 16:15:33 +0100

    Bump to v1.9.2-DEV

--- src/Application.php
+++ src/Application.php
@@ -18,7 +18,7 @@ use Symfony\Component\Console\Application as BaseApplication;
  */
 final class Application extends BaseApplication
 {
-    const VERSION = '0.9.1-DEV';
+    const VERSION = '0.9.2-DEV';
     const REPOSITORY_NAME = 'manala/manalize';
 
     public function __construct()
