b14d08bf7bbb2bfe00a0c306021f382a01161d38
Maxime Steinhausser <maxime.steinhausser@elao.com>
2019-07-26 09:17:15 +0200

    Bump version 0.10.1

--- src/Application.php
+++ src/Application.php
@@ -18,7 +18,7 @@ use Symfony\Component\Console\Application as BaseApplication;
  */
 final class Application extends BaseApplication
 {
-    const VERSION = '0.10.1-DEV';
+    const VERSION = '0.10.1';
     const REPOSITORY_NAME = 'manala/manalize';
 
     public function __construct()
